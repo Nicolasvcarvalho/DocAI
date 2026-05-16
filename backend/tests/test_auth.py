37b463a35a93fe08a9cd7ec8b393164baf1519c6
Nicolas <nikolas.vitor18@gmail.com>
2026-05-16 09:47:06 -0300

    test: login

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from uuid import uuid4
 
 from main import app
 
@@ -6,16 +7,27 @@ client = TestClient(app)
 
 def test_criar_candidato():
 
+    email = f"nicolas{uuid4()}@gmail.com"
+
     response = client.post(
         "/candidatos",
         json={
             "nome": "Nicolas",
             "sobrenome": "Carvalho",
             "sexo": "MASCULINO",
-            "email": "nicolas@gmail.com",
+            "email": email,
             "senha": "12345",
             "data_nascimento": "2025-10-22"
         }
+    )
+
+    assert response.status_code == 200
+
+def test_login():
+
+    response = client.post(
+        "/login",
+        json={"email": "nicolas@gmail.com", "senha": "12345"}
     )
 
     assert response.status_code == 200

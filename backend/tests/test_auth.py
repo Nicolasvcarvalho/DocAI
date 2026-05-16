3219e03c0d748245a56dea3804f3b58cfa5a9612
Nicolas <nikolas.vitor18@gmail.com>
2026-05-16 11:26:54 -0300

    test: testar email ja cadastrado

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -31,3 +31,39 @@ def test_criar_candidato_e_logar_candidato():
     )
 
     assert response.status_code == 200
+
+def test_email_duplicado():
+
+    email = f"nicolas{uuid4()}@gmail.com"
+    senha = "123"
+
+    response = client.post(
+        "/candidatos",
+        json={
+            "nome": "Nicolas",
+            "sobrenome": "Carvalho",
+            "sexo": "MASCULINO",
+            "email": email,
+            "senha": senha,
+            "data_nascimento": "2025-10-22"
+        }
+    )
+
+    assert response.status_code == 200
+
+    response = client.post(
+        "/candidatos",
+        json={
+            "nome": "Nicolas",
+            "sobrenome": "Carvalho",
+            "sexo": "MASCULINO",
+            "email": email,
+            "senha": senha,
+            "data_nascimento": "2025-10-22"
+        }
+    )
+
+    assert response.status_code == 400
+    assert response.json()['detail'] == "Email já cadastrado"
+
+    

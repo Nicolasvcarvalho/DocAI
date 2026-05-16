74f9845b04ddf61768ff9b5f984a45a3280f8caf
Nicolas <nikolas.vitor18@gmail.com>
2026-05-16 11:33:11 -0300

    test: testar senha incorreta

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -66,4 +66,29 @@ def test_email_duplicado():
     assert response.status_code == 400
     assert response.json()['detail'] == "Email já cadastrado"
 
+def test_senha_errada():
+
+    email = f"nicolas{uuid4()}@gmail.com"
+    senha = "123"
+
+    response = client.post(
+        "/candidatos",
+        json={
+            "nome": "Nicolas",
+            "sobrenome": "Carvalho",
+            "sexo": "MASCULINO",
+            "email": email,
+            "senha": senha,
+            "data_nascimento": "2025-10-22"
+        }
+    )
+
+    assert response.status_code == 200
     
+    response = client.post(
+        "/login",
+        json={"email": email, "senha": "senha_errada"}
+    )
+
+    assert response.status_code == 401
+    assert response.json()['detail'] == "Email ou senha inválidos"

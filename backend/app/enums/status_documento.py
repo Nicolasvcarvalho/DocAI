500a46cee29b8a45762b302f59728c236bd9ff72
Nicolas <nikolas.vitor18@gmail.com>
2026-05-20 22:24:42 -0300

    feat: implementa workflow documental

--- backend/app/enums/status_documento.py
+++ backend/app/enums/status_documento.py
@@ -5,6 +5,10 @@ class StatusDocumento(str, Enum):
     PENDENTE_ENVIO = "PENDENTE_ENVIO"
     ENVIADO = "ENVIADO"
     PROCESSANDO = "PROCESSANDO"
+    AGUARDANDO_CONFIRMACAO = "AGUARDANDO_CONFIRMACAO"
     PROCESSADO = "PROCESSADO"
+    EM_ANALISE = "EM_ANALISE"
     APROVADO = "APROVADO"
-    REJEITADO = "REJEITADO"  
+    REJEITADO = "REJEITADO" 
+
+    # Add o processado para se referir o momento em que foi  processado mas nao aanalisado   

4e7bc5aa9300213d49557a9be6cbb6bcaecf2602
Nicolas <nikolas.vitor18@gmail.com>
2026-06-17 17:44:43 -0300

    refactor: limpeza do metodo de calculo de candidatura

--- backend/app/services/documento/workflow/candidatura_workflow.py
+++ backend/app/services/documento/workflow/candidatura_workflow.py
@@ -7,34 +7,31 @@ class CandidaturaWorkflowService:
 
     @staticmethod
     def recalcular_status_candidatura(candidatura: Candidatura) -> StatusCandidatura:
-        
-        documentos = candidatura.documentos
-
-        if not documentos:
             
-            return StatusCandidatura.AGUARDANDO_DOCUMENTOS
-        
-        documentos_obrigatorios = [documento for documento in documentos if documento.tipo_documento.obrigatorio_base]
+            documentos = candidatura.documentos
 
-        status_presentes = {documento.status for documento in documentos_obrigatorios}
+            if not documentos:
+                
+                return StatusCandidatura.AGUARDANDO_DOCUMENTOS
+            
+            documentos_obrigatorios = [documento for documento in documentos if documento.tipo_documento.obrigatorio_base]
 
-        status_esperando_candidato = {StatusDocumento.PENDENTE_ENVIO, StatusDocumento.AGUARDANDO_REENVIO}
+            status_presentes = {documento.status for documento in documentos_obrigatorios}
 
-        if status_presentes.intersection(status_esperando_candidato):
-            return StatusCandidatura.AGUARDANDO_DOCUMENTOS
+            status_esperando_candidato = {StatusDocumento.PENDENTE_ENVIO, StatusDocumento.AGUARDANDO_REENVIO}
 
-        if StatusDocumento.PENDENTE_ENVIO in status_presentes:
-            return StatusCandidatura.DOCUMENTACAO_PENDENTE
-        
-        status_processamento = {StatusDocumento.ENVIADO, StatusDocumento.PROCESSANDO, StatusDocumento.AGUARDANDO_CONFIRMACAO}
+            if status_presentes.intersection(status_esperando_candidato):
+                return StatusCandidatura.AGUARDANDO_DOCUMENTOS
+            
+            status_processamento = {StatusDocumento.ENVIADO, StatusDocumento.PROCESSANDO, StatusDocumento.AGUARDANDO_CONFIRMACAO}
 
-        if status_presentes.intersection(status_processamento):
-            return StatusCandidatura.DOCUMENTACAO_EM_PROCESSAMENTO
-        
-        if StatusDocumento.EM_ANALISE in status_presentes:
-            return StatusCandidatura.EM_ANALISE if candidatura.possui_analista else StatusCandidatura.DOCUMENTACAO_PENDENTE
-        
-        if StatusDocumento.APROVADO == status_presentes:
-            return StatusCandidatura.APROVADA
-        
-        raise ValueError(f"Status de documentos não mapeado: {status_presentes}")
+            if status_presentes.intersection(status_processamento):
+                return StatusCandidatura.DOCUMENTACAO_EM_PROCESSAMENTO
+            
+            if StatusDocumento.EM_ANALISE in status_presentes:
+                return StatusCandidatura.EM_ANALISE if candidatura.possui_analista else StatusCandidatura.DOCUMENTACAO_PENDENTE
+            
+            if StatusDocumento.APROVADO == status_presentes:
+                return StatusCandidatura.APROVADA
+            
+            raise ValueError(f"Status de documentos não mapeado: {status_presentes}")

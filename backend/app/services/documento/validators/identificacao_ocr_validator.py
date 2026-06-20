672b4d959bc6d50f779b0e86368d4abf998baf84
Nicolas <nikolas.vitor18@gmail.com>
2026-06-19 21:32:19 -0300

    feat: integrar validacoes em doc identificacao

--- backend/app/services/documento/validators/identificacao_ocr_validator.py
+++ backend/app/services/documento/validators/identificacao_ocr_validator.py
@@ -1,5 +1,20 @@
 from app.services.documento.validators.ocr_validator import OCRValidator
+from app.services.documento.validators.field_validator import FieldValidator
 
 class DocumentoIdentificacaoOCRValidator(OCRValidator):
 
     CAMPOS_OBRIGATORIOS = ["nome", "rg", "cpf", "data_nascimento", "nome_pai", "nome_mae"]
+
+    @classmethod
+    def validar(cls, dados: dict):
+        
+        super().validar(dados)
+
+        dados["cpf"] = FieldValidator.validar_cpf(dados["cpf"])
+
+        dados["rg"] = FieldValidator.validar_rg(dados["rg"])
+
+        dados["data_nascimento"] = FieldValidator.validar_data_nascimento(dados["data_nascimento"])
+
+        return dados
+    

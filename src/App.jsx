099e5351e26a2a543085843544ca5cfaebbcf8bd
Guthierry Sousa <franciscoguthierry750@gmail.com>
2026-06-20 13:06:04 -0300

    feat: add aplication routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Cadastro from './pages/Cadastro/Cadastro'
-import DashboardCandidato from './pages/Dashboard/DashboardCandidato';
+import DashboardCandidato from './pages/Candidato/DashboardCandidato'
+import DashboardSecretaria from './pages/Secretaria/DashboardSecretaria';
+import AnaliseCandidatura from './pages/Secretaria/AnaliseCandidatura';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/dashboard-candidato" element={<DashboardCandidato />} />
+        <Route path="/dashboard-secretaria" element={<DashboardSecretaria />} />
+        <Route path="/secretaria/candidatura/:candidaturaId" element={<AnaliseCandidatura />} />
       </Routes>
     </BrowserRouter>
   );
